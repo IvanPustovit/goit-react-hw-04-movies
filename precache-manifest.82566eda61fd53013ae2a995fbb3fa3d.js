9305818789c25b0973e4f235b992f5e660444e99
IvanPustovit <pustovit.for@gmail.com>
2020-05-14 15:35:21 +0300

    Updates

---
+++ precache-manifest.82566eda61fd53013ae2a995fbb3fa3d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0710de82b62300e1aa42c0a2fcf2b303",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "4ef94008ea15c54c478b",
+    "url": "/goit-react-hw-04-movies/static/css/main.7a38558f.chunk.css"
+  },
+  {
+    "revision": "97aad119eba96589ad78",
+    "url": "/goit-react-hw-04-movies/static/js/0.7a4cf55b.chunk.js"
+  },
+  {
+    "revision": "d34d048ee33d961d9205",
+    "url": "/goit-react-hw-04-movies/static/js/3.3e065ba2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/3.3e065ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5321f77537587753c07",
+    "url": "/goit-react-hw-04-movies/static/js/4.7041dd2e.chunk.js"
+  },
+  {
+    "revision": "9b4b01f5d98df3d8b2f0",
+    "url": "/goit-react-hw-04-movies/static/js/5.d45e89cd.chunk.js"
+  },
+  {
+    "revision": "8575dba01281d2d5595b",
+    "url": "/goit-react-hw-04-movies/static/js/6.59e06fb3.chunk.js"
+  },
+  {
+    "revision": "a7c51b9d6e6603e2f766",
+    "url": "/goit-react-hw-04-movies/static/js/7.8b4df2c6.chunk.js"
+  },
+  {
+    "revision": "054c041e04c82d05be52",
+    "url": "/goit-react-hw-04-movies/static/js/8.fe3efee9.chunk.js"
+  },
+  {
+    "revision": "4ef94008ea15c54c478b",
+    "url": "/goit-react-hw-04-movies/static/js/main.5477938b.chunk.js"
+  },
+  {
+    "revision": "73cf3a26563cf62dfc5b",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.77bc23a7.js"
+  }
+]);
